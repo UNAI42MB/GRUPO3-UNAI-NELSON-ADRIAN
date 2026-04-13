1b3bc6ddd86afda7820816e6bb18226401361a69
Adrian <aarmengodcajal@gmail.com>
2026-04-13 12:00:57 +0200

    Agrego prueba de conexion de NeatBeans y MySQL

---
+++ Programacion/Prueba_MySQL/src/prueba_mysql/dbConnection.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba_mysql;
+import java.sql.*;
+
+/**
+ *
+ * @author adria
+ */
+public class dbConnection {
+    static String url = "jdbc:mysql://localhost:3306/instituto_avanzado";
+    static String user = "root";
+    static String pass = "Adrititan2007";
+    
+    
+    public static Connection conectar(){
+        
+        Connection con = null;
+        try{
+            con =DriverManager.getConnection(url,user,pass);
+            System.out.println("Conexion exitosa");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+    
+}

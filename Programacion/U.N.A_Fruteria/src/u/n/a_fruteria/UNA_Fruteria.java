134dcb5f4991234c24c1822d3a0ef2ab00d89bd9
Adrian <aarmengodcajal@gmail.com>
2026-04-15 10:59:47 +0200

    Agrego primer cambio en el programa

---
+++ Programacion/U.N.A_Fruteria/src/u/n/a_fruteria/UNA_Fruteria.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package u.n.a_fruteria;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author adria
+ */
+public class UNA_Fruteria extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+       Pantalla_fruteria objeto_pantalla = new Pantalla_fruteria();
+       objeto_pantalla.setVisible(true);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        dbConnection dbc = new dbConnection();
+        dbc.conectar();
+     
+    
+}}
